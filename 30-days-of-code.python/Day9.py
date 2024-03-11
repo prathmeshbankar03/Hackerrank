776e2ee9c06e4e643765045bc8e471701e924182
prathmeshbankar03 <prathmeshbankar03@gmail.com>
2024-03-11 23:41:10 +0530

    Day 9 Done✅

---
+++ 30-days-of-code.python/Day9.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'factorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def factorial(n):
+    # Write your code here
+    num=1
+    for i in range(1,n+1):
+        num=num*i
+    return num
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
